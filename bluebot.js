03388281eb4e5c3a8d76d06824805fda6300232e
Andrew <covadax.ag@gmail.com>
2020-09-22 16:35:04 -0400

    removed vestigial code

--- bluebot.js
+++ bluebot.js
@@ -4,26 +4,15 @@ require('dotenv').config();
 const Discord = require('discord.js');
 // create a new Discord client
 const client = new Discord.Client();
-const blu_words = [ "blu", "blue"]
-
-let ver = process.env.NODE_ENV;
 
 //login to Discord with your app's token
 client.login()
 
-// Listens for any messages that contain "blue" or "blu " and replies with an obnoxious message.
+// Listens for any messages that contain "blu " and replies with an obnoxious message.
 client.on('message', msg => {
     if (msg.author.id != client.user.id) {
-        if (blu_words.some(word => msg.content.toLowerCase().includes(word))) {
+        if (msg.content.toLowerCase().includes('blu')) {
             msg.channel.send('Did somebody say BLU?')
         } 
-    }
-});
-
-client.on('ready', () => {
-    if (ver == 'production') {
-        console.log("Prod");
-    } else {
-        console.log(ver);
     }
 });
